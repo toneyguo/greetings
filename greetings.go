246254dfe01b78b5bd7bfb81ee2eb9b524127e7f
toneyguo <toney.guo@outlook.com>
2024-03-16 17:14:34 +0800

    3

--- greetings.go
+++ greetings.go
@@ -3,12 +3,22 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	format := []string{
+		"hi %v welcome!",
+		"great to see you %v",
+		"hail,%v ! well met",
+	}
+	return format[rand.Intn(len(format))]
 }
